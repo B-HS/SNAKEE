70a36181a534610e9b726a6c4f11682cd51e15ff
B-HS <gumyoincirno@gmail.com>
2022-07-03 23:06:45 +0900

    full change for game

--- js/index.js
+++ js/index.js
@@ -1,79 +1,122 @@
-const canvas = document.getElementById('canvas');
-let currentX = 0
-let currentY = 0
-let prevX = 0
-let prevY = 0
-function draw(x1, y1, a, b) {
-	const ctx = canvas.getContext('2d');
-	ctx.fillStyle = 'green';
-	ctx.fillRect(50 * x1, 50 * y1, a, b);
-	currentX = x1
-	currentY = y1
-	
+let ctx
+let cvas
+let width
+let height
+let blockSize= 25
+let rows =20
+let cols =20
+let x = blockSize*1
+let y = blockSize*1
+let fx;
+let fy;
+let vx = 0
+let vy = 0
+let speed = 10
+let tail = []
+let gameOver = false
+window.onload = function(){
+    cvas = document.getElementById("canvas")
+    ctx = cvas.getContext("2d")
+    cvas.height= rows * blockSize
+    cvas.width = cols * blockSize
+    genFood()
+    document.addEventListener("keyup", objectDirect)
+    document.addEventListener("keydown", resnake)
+    // snake()
+    setInterval(snake, 1000/speed)
 }
-function unDraw(x1, y1, a, b){
-	const ctx = canvas.getContext('2d');
-	ctx.fillStyle = 'white';
-	ctx.fillRect(50 * x1, 50 * y1, a, b);
+function objectDirect(e){
+    switch(e.key){
+        case "ArrowUp":
+            if (vy!=1){vx = 0; vy = -1;}
+            break
+        case "ArrowDown":
 
-	if(x1<0){
-		
-	}
-
+            if (vy!=-1){vx = 0; vy = 1;}
+            break
+        case "ArrowLeft":
+            if (vx!=1){vx = -1; vy = 0;}
+            break
+        case "ArrowRight":
+            if (vx!=-1){vx = 1 ;vy = 0;}
+            break
+    }
+        
 }
-
-function genFed(){
-
-}
-let DIRECTION = ""
-function recogKey(e, dirc) {
-	if (dirc == "") { dirc = e.key } else { e.key = dirc }
-	switch (e.key) {
-		case "ArrowDown":
-			prevX = currentX
-			prevY = currentY
-			
-			draw(currentX, currentY + 1, wBoxSize, hBoxSize)	
-			unDraw(prevX, prevY, wBoxSize, hBoxSize)
-			console.log("ArrowDown");
-			DIRECTION = "ArrowDown"
-			break;
-		case "ArrowUp":
-			draw(currentX, currentY - 1, wBoxSize, hBoxSize)
-			unDraw(prevX, prevY, wBoxSize, hBoxSize)
-			console.log("ArrowUp");
-			DIRECTION = "ArrowUp"
-			break;
-		case "ArrowLeft":
-			draw(currentX - 1, currentY, wBoxSize, hBoxSize)
-			unDraw(prevX, prevY, wBoxSize, hBoxSize)
-			console.log("ArrowLeft");
-			DIRECTION = "ArrowLeft"
-
-			break;
-		case "ArrowRight":
-			draw(currentX + 1, currentY, wBoxSize, hBoxSize)
-			unDraw(prevX, prevY, wBoxSize, hBoxSize)
-			console.log("ArrowRight");
-			DIRECTION = "ArrowRight"
-			break;
-		default:
-			console.log(e.key, e.keyCode);
-			return;
-	}
-	prevX = currentX
-	prevY = currentY
-	console.log(prevX, prevY, currentX, currentY);
+function genFood(){
+    fx = Math.floor(Math.random()*cols)*blockSize
+    fy = Math.floor(Math.random()*rows)*blockSize
 }
 
 
 
-const wBoxSize = 50
-const hBoxSize = 50
-draw(0, 0, 50, 50)
-addEventListener("keydown", recogKey)
-let cnt = 0
+function snake(){ 
+    if(gameOver){
+        return
+    }
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, cvas.width, cvas.height)
+    
+    ctx.fillStyle="red"
+    ctx.fillRect(fx, fy, blockSize, blockSize)
+    //Generate food when object reached to food and add tail
+    if (x==fx&& y==fy){
+        genFood()
+        tail.push([fx, fy])
+    }
+    //Move array's last object to -1 array for moving snake
+    for(let i = tail.length-1; i>0; i-- ){
+        tail[i] = tail[i-1]
+    }
+    //set head
+    if(tail.length){
+        tail[0] = [x, y]
+    }
+    //set initial snake
+    x+=vx*blockSize
+    y+=vy*blockSize
+    ctx.fillStyle="lime" 
+    ctx.fillRect(x, y, blockSize, blockSize)
+    
+    
+    //making tail with head
+    for(let i = 0; i<tail.length;i++){
+        ctx.fillStyle="lime"
+        ctx.fillRect(tail[i][0], tail[i][1], blockSize, blockSize)
+    }
 
+    //condition for game over
+    if(x<0 || x>=cols*blockSize || y<0|| y>=rows*blockSize){
+        gameOver=true
+        alert("gameOver")
+        
+
+    }
+    //condition for game over
+    for(let i =0; i<tail.length; i++){
+        if(x==tail[i][0] && y ==tail[i][1]){
+            gameOver=true;
+            alert("gameOver")
+
+        }
+    }
+
+    }
+        //reset
+        function resnake(e){
+            if(e.keyCode == "32"){
+            x = blockSize*1
+            y = blockSize*1
+            genFood()
+            vx = 0
+            vy = 0
+            speed = 15
+            tail = []
+            gameOver = false
+            snake()
+        }
+
+}
 
 
 
